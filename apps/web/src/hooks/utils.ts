74f5a53bdfdf819cab53d227858d93aab13ab877
Rutishkrishna Srinivasaraghavan <ucabr02@ucl.ac.uk>
2025-03-11 01:38:52 +0000

    Update utils.ts

--- apps/web/src/hooks/utils.ts
+++ apps/web/src/hooks/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from "@langchain/langgraph-sdk";
 
 export const createClient = () => {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? "/api";
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? "https://open-canvas-web-two.vercel.app/api";
   return new Client({
     apiUrl,
   });
